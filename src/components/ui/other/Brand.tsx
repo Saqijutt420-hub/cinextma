1af98e3f49e5a6265cb8e29b832e3ae9200bff1b
Saqijutt420-hub <ukpro308@gmail.com>
2025-03-31 05:09:50 +0200

    Update Brand.tsx

--- src/components/ui/other/Brand.tsx
+++ src/components/ui/other/Brand.tsx
@@ -7,11 +7,11 @@ export default function Brand() {
     <div>
       <Link href="/" className={Saira.className} style={{ fontWeight: 600, fontSize: 30 }}>
         <span className="flex items-center tracking-widest">
-          CINE{" "}
+          FM{" "}
           <span>
             <TbPlayerTrackNextFilled className="size-full px-[2px] text-primary" />
           </span>{" "}
-          TMA
+          VIES
         </span>
       </Link>
     </div>
